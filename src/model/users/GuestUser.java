3c6dd334500e8180a806385367805154ed9e8929
ValdisEizans <valdis.eizans@gmail.com>
2026-03-17 11:58:36 +0200

    Edit: Guestuser class created and completed

---
+++ src/model/users/GuestUser.java
@@ -0,0 +1,34 @@
+package model.users;
+
+public class GuestUser {
+	//Mainigie
+	private long id;
+	private static long counter = 1;
+	
+	//Geteri
+	public long getId() {
+		return id;
+	}
+
+	//Seteri
+	public void setId() {
+		id = counter++;
+	}
+	
+	
+	//Konstruktori
+	public GuestUser() {
+		setId();
+	}
+	//saja klase argumentu konstruktoru nevajag, jo nav user datu
+	
+	//toString
+	public String toString() {
+		String result = "" + id;
+		return result;
+	}
+	
+	//TO DO papildinat ar find servisiem
+	
+
+}
